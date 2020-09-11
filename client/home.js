f060655e4f9f647c2ba8243fdd07e01b2013dd06
Samartsev Igor <igorsamartsev@gmail.com>
2020-09-11 18:30:35 +0400

    update

--- client/home.js
+++ client/home.js
@@ -1,17 +1,9 @@
 import React from "react"
-import {Route} from "react-router-dom";
-import Navbar from './navbar'
-import AddUser from "./myUsers";
-import User from "./user";
-import AllUsers from "./all-users";
 
 const Home = () => {
   return (
     <div>
-      <Navbar />
-      <AddUser />
-      <Route exact path='/user' component={() => <User />} />
-      <Route exact path='/allusers' component={() => <AllUsers />} />
+      This is home
     </div>
   )
 }
--- client/root.js
+++ client/root.js
@@ -1,21 +1,14 @@
 import React from 'react'
-import {Provider} from 'react-redux'
 import {BrowserRouter, Route, Switch} from 'react-router-dom'
-import store from './redux/index'
-import Home from "./home";
-
+import Home from './home'
 
 const Root = () => {
   return (
-    <Provider store={store}>
-      <BrowserRouter>
-        <Switch>
-          <Route exact path='/' component={() => <Home />} />
-          <Route exact path='/user' component={() => <Home />} />
-          <Route exact path='/allusers' component={() => <Home />} />
-        </Switch>
-      </BrowserRouter>
-    </Provider>
+    <BrowserRouter>
+      <Switch>
+        <Route exact path='/' component={() => <Home />} />
+      </Switch>
+    </BrowserRouter>
   )
 }
 
